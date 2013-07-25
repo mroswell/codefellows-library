f8fe68fc1368c518d2ebef849609c5c28c22c9fe
Marjorie Roswell <mroswell@gmail.com>
2013-07-25 08:47:38 -0400

    add and remove shelves (commented out)

--- ruby-library.rb
+++ ruby-library.rb
@@ -1,47 +1,54 @@
 #!/usr/local/bin/ruby -w
 
-# Bad Ruby code, written BEFORE taking the training. Guesses. But I always did love books.
+# Bad Ruby code, written BEFORE taking the training. 
+# Googling, plus guesses. 
 
 # define Library, Shelf, and Book classes
-class Library
-   # constructor
-   def initialize(libname, numshelves)
-     @libname = libname
-     @numshelves = numshelves
-   end
 
-   # print list of books within the library (or try to!)
-   def listBooks
-    books.each do |book|
-      puts book
-    end
+class Library
+  def initialize(libname, numshelves)
+   @libname = libname
+   @numshelves = numshelves
   end
-  
-  def countBooks
-    bookcount = books.length
+
+  # print list of books within the library (or try to!)
+  def listBooks
+   books.each do |book|
+    puts book
+   end
+	end
+
+  def countshelves
+  puts shelfs.length
   end
 end
 
+=begin
+  # not sure if we add and remove a shelf from the library class or the shelf class
+	def addShelf(shelf)
+	this.push(shelf)
+	end
+	
+	def removeShelf(shelf)
+    this.delete(shelf)  
+	end
+=end
+	
 class Shelf
-   # constructor 
    def initialize(shelfname, booksonshelf)
       @shelfname = shelfname
-      booksonshelf = []
-      @books = booksonshelf
+      @booksonshelf = booksonshelf
    end
-
 end
 
 class Book
-  
-   # constructor
    def initialize(bookname, shelf="not-shelved")
     @bookname = bookname
     @shelf = shelf
    end
    
    def enshelf(shelf)
-      @shelf = shelf
+      this.setShelf(shelf)
    end
 
    def unshelf
@@ -49,7 +56,8 @@ class Book
    end
 end
 
-#Instantiate Library, Shelf, and Book 
+# Instantiate Library, Shelf, and Book 
 library = Library.new("Old Alexandria", 54321)
 shelf = Shelf.new("Nutrition",500)
 rawbook1  = Book.new("Raw Food Made Easy", "Nutrition") 
+
